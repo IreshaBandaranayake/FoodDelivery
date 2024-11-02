7b36fee16cc31ee7705a403dd6b3c7a9e5139122
Iresha Bandaranayake <ireshabke@gmail.com>
2024-11-02 23:15:43 +0200

    Done Changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className='header'>
         <div className='header-contents'>
-            <h2>Order your favourite foods here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the favourite meals. Delicious Meals </p>
+            <h2>Crave it, order now!</h2>
+            <p>Discover a menu brimming with diverse, expertly crafted dishes. Savor your favorite meals, prepared to perfection! </p>
             <button>View Menu</button>
         </div>
     </div>
