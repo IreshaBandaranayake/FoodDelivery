b1a876f698e9f80f88a23b5d1475493162e07df7
Iresha Bandaranayake <ireshabke@gmail.com>
2024-11-04 01:41:09 +0200

    Done Changes

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Mealhub is a food delivery website. I'm developing this for my Full stack development project</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>Get In Touch</h2>
+                <ul>
+                    <li>+358442349874</li>
+                    <li>contact@mealhub.com</li>    
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright 2024 &copy; mealhub.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/LoginPopup/LogiPopup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LogiPopup.css'
+
+const LogiPopup = () => {
+  return (
+    <div className='login-popup'></div>
+  )
+}
+
+export default LogiPopup
