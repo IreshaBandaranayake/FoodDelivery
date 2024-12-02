41aa9a0d4a90e187bb47fc35b0ad39f7147a109d
Iresha Bandaranayake <ireshabke@gmail.com>
2024-12-03 01:50:56 +0200

    done changes

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -34,7 +34,7 @@ const LoginPopup = ({setShowLogin}) => {
 
       if(response.data.success){
         setToken(response.data.token);
-        localStorage.setItem("token",response.data.token)<
+        localStorage.setItem("token",response.data.token);
         setShowLogin(false)
       }
       else{
