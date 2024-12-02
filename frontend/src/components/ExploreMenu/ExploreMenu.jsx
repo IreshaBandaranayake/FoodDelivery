876d3189e9c7cc20ac848e496d111995691391d5
Iresha Bandaranayake <ireshabke@gmail.com>
2024-12-02 21:37:06 +0200

    Done Changes

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = ({category,setCategory}) => {
 
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore our menu</h1>
+        <h1>Find Your Favorite</h1>
         <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array</p>
         <div className='explore-menu-list'>
           {menu_list.map((item,index)=>{
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     <div className='header'>
         <div className='header-contents'>
             <h2>Crave it, order now!</h2>
-            <p>Discover a menu brimming with diverse, expertly crafted dishes. Savor your favorite meals, prepared to perfection! </p>
-            <button><a href="#explore-menu">View Menu</a></button>
+            <p>Discover a menu brimming with diverse, expertly crafted dishes, designed to delight every palate. Savor your favorite meals, prepared to perfection, and explore new flavors that promise to surprise and satisfy.</p>
+            <button><a href="#explore-menu">View Our Menu</a></button>
         </div>
     </div>
   )
