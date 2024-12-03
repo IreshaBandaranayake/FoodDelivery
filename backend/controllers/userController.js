7bce2673ee64b1bbbc767a59e9c87e76c7df8190
Iresha Bandaranayake <ireshabke@gmail.com>
2024-12-03 12:54:29 +0200

    Done changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+
 import validator from "validator"
 
 
